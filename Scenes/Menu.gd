ad0d527e681cf343b7bbf03bc3f65e6c8cb4d489
sammit52 <sammit52@kingshigh.school.nz>
2022-10-19 16:35:44 +1300

    Improvement To Menu

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -1,13 +1,34 @@
 extends Control
 
+func _ready():
+	$FillerButton.grab_focus()
+
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://Scenes/World.tscn")
 	
-
 func _on_OptionsButton_pressed():
-	pass # Replace with function body.
-
+	get_tree().change_scene("res://Options.tscn")
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+func _on_StartButton_mouse_entered():
+		$Buttons/StartButton.grab_focus()
+
+func _on_OptionsButton_mouse_entered():
+		$Buttons/OptionsButton.grab_focus()
+
+func _on_QuitButton_mouse_entered():
+		$Buttons/QuitButton.grab_focus()
+		
+func _on_QuitButton_mouse_exited():
+	$FillerButton.grab_focus()
+
+func _on_OptionsButton_mouse_exited():
+	$FillerButton.grab_focus()
+
+func _on_StartButton_mouse_exited():
+	$FillerButton.grab_focus()
+	
+
 
