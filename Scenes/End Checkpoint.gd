c006b8231d0f007927e9f3ff7007c572e96a1115
sammit52 <sammit52@kingshigh.school.nz>
2022-10-20 12:20:56 +1300

    Options And End Scene

--- Scenes/End Checkpoint.gd
+++ Scenes/End Checkpoint.gd
@@ -2,4 +2,9 @@ extends Node2D
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
-		get_tree().change_scene("res://End Scene.tscn")
+		get_tree().change_scene("res://EndScene.tscn")
+
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group("Player"):
+		get_tree().change_scene("res://EndScene.tscn")
